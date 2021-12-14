bc170ee0900bccc1b2d26e22350a7d19c69e75d1
philippegerber <philippe.gerber@hydrique.ch>
2021-12-14 13:53:03 +0100

    Isolated email sending

---
+++ src/AmisDeNoel/FriendEmailSender.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace AmisDeNoel
+{
+    public static class FriendEmailSender
+    {
+        private const string GiverPlaceholder = ":::Giver:::";
+        private const string ReceiverPlaceholder = ":::Receiver:::";
+        internal static void SendEmails(List<ChristmasMatch> matches, string htmlTemplatePath, string user, string pass)
+        {
+            var htmlTemplate = File.ReadAllText(htmlTemplatePath);
+
+            foreach (var match in matches)
+            {
+                var htmlContent = 
+                        htmlTemplate
+                            .Replace(GiverPlaceholder, match.Giver.Name)
+                            .Replace(ReceiverPlaceholder, match.Receiver.Name);
+
+                try
+                {
+                    using (var smtpClient = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        smtpClient.UseDefaultCredentials = false;
+                        smtpClient.Credentials = new NetworkCredential()
+                        {
+                            UserName = user,
+                            Password = pass,
+                        };
+                        smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                        smtpClient.EnableSsl = true;
+                        //match.Giver.Email
+                        var mail = new MailMessage(user, "philippe.e.gerber@gmail.com", "Oh oh oh", htmlContent);
+                        mail.IsBodyHtml = true;
+                        smtpClient.Send(mail);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine("{0}: {1}", e.ToString(), e.Message);
+                }
+            }
+        }
+    }
+}
